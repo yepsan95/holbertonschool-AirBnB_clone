5c2f688596cced4b00214c8dbc252a97ee42f76b
Bryan Yep <yepsan95@gmail.com>
2023-08-15 09:49:24 -0700

    feat: added BaseModel class.

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Defines the BaseModel class.
+"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Represents the base model for the AirBnB clone project.
+    """
+
+   def __init__(self):
+       """
+       Initializes a new instance of BaseModel.
+       """
+
+       self.id = str(uuid4())
+       self.created_at = datetime.today()
+       self.updated_at = datetime.today()
+
+    def save(self):
+        """
+        Updates the attribute updated_at with the current datetime.
+        """
+
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all key/values of __dict__
+        of the instance.
+        """
+
+        dict = self.__dict__.copy()
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
+        dict["__class__"] = self.__class__.__name__
+
+        return dict
+
+    def __str__(self):
+        """
+        Returns the string representation of hte object.
+        """
+
+        print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
