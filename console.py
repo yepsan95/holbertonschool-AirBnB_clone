334b3c565658dbd3f4328042fc348b7b016c3cc4
Bryan Yep <yepsan95@gmail.com>
2023-08-19 21:54:33 -0700

    feat: added the command interpreter for the HBNB project.

---
+++ console.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Defines the HBNB command interpreter.
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Command interpreter for the Holberton AirBnb clone project.
+
+    Attributes:
+        prompt (str): The prompt to be shown in the command interpreter.
+    """
+
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """
+        Do nothing when receiving an empty line.
+        """
+
+        pass
+
+    def do_quit(self, arg):
+        """
+        Exits the program.
+        """
+
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF signal to exit the program.
+        """
+
+        print("")
+        return True
